0691802db40833c9294632280a6c13d961804b87
Francisco I. Rodriguez <firodriguez@dcccd.edu>
2021-11-09 06:27:08 -0600

    Create while.java

---
+++ week04/while.java
@@ -0,0 +1,18 @@
+/* Java Program Example - Java while Loop
+ * Demonstrate the while Loop */
+
+public class JavaProgram
+{   
+    public static void main(String args[])
+    {
+        
+        int n = 10;
+        
+        while(n>0)
+        {
+            System.out.println("tick " + n);
+            n--;
+        }
+        
+    }
+}
