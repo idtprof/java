87e47fe11c6ba5bd9d867b0ebbada140eabc7973
Francisco I. Rodriguez <firodriguez@dcccd.edu>
2021-11-09 08:19:10 -0600

    on and off

--- week04/JavaProgram.java
+++ week04/JavaProgram.java
@@ -1,20 +1,61 @@
-/* Java Program Example - Java while Loop
- * The target of a loop can be empty */
+/* Java Program Example - Java do-while Loop 
+ * Using a do-while loop to process a menu selection 
+ */
+
+import java.util.Scanner;
 
 public class JavaProgram
 {   
     public static void main(String args[])
     {
         
-        int i, j;
+        Scanner scan = new Scanner(System.in);
         
-        i = 100;
-        j = 200;
+        char choice;
         
-        // find midpoint between i and j
-        while(++i < --j);    // there is no body in this loop
+        do
+        {
+            System.out.println("Help on : ");
+            System.out.println("1. if");
+            System.out.println("2. switch");
+            System.out.println("3. while");
+            System.out.println("4. do-while");
+            System.out.println("5. for\n");
+            
+            System.out.println("Choose any one : ");
+            choice = scan.next().charAt(0);
+        }while(choice < '1' || choice > '5');
         
-        System.out.println("Midpoint is " + i);
+        System.out.println("\n");
+        
+        switch(choice)
+        {
+            case '1' : System.out.println("The if :\n");
+                System.out.println("if(condition)\n{\n\tstatement\n}");
+                System.out.println("else\n{\n\tstatement\n}");
+                break;
+            
+            case '2' : System.out.println("The switch :\n");
+                System.out.println("switch(expression)\n{");
+                System.out.println("\tcase constant: statement sequence\n\tbreak;");
+                System.out.println("\t//...\n}");
+                break;
+                
+            case '3' : System.out.println("The while :\n");
+                System.out.println("while(condition)\n{");
+                System.out.println("\t// body of loop\n}");
+                break;
+                
+            case '4' : System.out.println("The do-while :\n");
+                System.out.println("do\n{");
+                System.out.println("\t// body of loop\n\n}while(condition);");
+                break;
+            
+            case '5' : System.out.println("The for :\n");
+                System.out.println("for(initialization; condition; iteration)\n{");
+                System.out.println("\t// body of loop\n}");
+                break;
+        }
         
     }
 }
