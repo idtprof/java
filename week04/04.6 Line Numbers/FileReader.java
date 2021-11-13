06a8fca1b6041d4f0e48e34b25cab99ea8c79e1e
Francisco I. Rodriguez <firodriguez@dcccd.edu>
2021-11-13 15:26:01 -0600

    second try

--- week04/04.6 Line Numbers/FileReader.java
+++ week04/04.6 Line Numbers/FileReader.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 public class FileReader {
     public static void main(String[] args) throws IOException {
 
-        String filename = "poetry.txt";
+        String filename;
         int lineCount = 1;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the file name: ");
+        filename = keyboard.nextLine();
 
         File file = new File(filename);
 
